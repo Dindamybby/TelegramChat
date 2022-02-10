98d4cee8a7505b44534fbd31b1a5c61c4c919782
Dindamybby <96799064+Dindamybby@users.noreply.github.com>
2022-02-10 16:56:35 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TOKEN = os.environ.get("TOKEN")
-OWNER = os.environ.get("OWNER")
+TOKEN = os.environ.get("5122084606:AAFYfeAQFhfcDRn3QcG8dcp0Y4YjKWQNSvk")
+OWNER = os.environ.get("@purwad1")
 GROUP = os.environ.get("GROUP")
 CHANNEL = os.environ.get("CHANNEL")
